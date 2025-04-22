16ef4a6b49eda8213117e3c3606f404d9c43680d
hmzah <hmzah.liaqat@gmail.com>
2025-04-22 14:34:06 -0400

    basic app2 functionality

---
+++ bootstrap/cache/modules.php
@@ -0,0 +1,19 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Dossier\\Providers\\DossierServiceProvider',
+    1 => 'Modules\\Dossier\\Providers\\RouteServiceProvider',
+    2 => 'Modules\\Samajhlo\\Providers\\SamajhloServiceProvider',
+    3 => 'Modules\\Samajhlo\\Providers\\RouteServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Dossier\\Providers\\DossierServiceProvider',
+    1 => 'Modules\\Dossier\\Providers\\RouteServiceProvider',
+    2 => 'Modules\\Samajhlo\\Providers\\SamajhloServiceProvider',
+    3 => 'Modules\\Samajhlo\\Providers\\RouteServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
